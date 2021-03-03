e80316897dd4344d4f16da62f617ee39e6fc565f
josh-seabaugh <joshua.seabaugh@colorado.edu>
2021-03-02 23:45:16 -0700

    update implementing the comments from Sam

--- Labs/Lab04/Seabaugh_Lab4.R
+++ Labs/Lab04/Seabaugh_Lab4.R
@@ -4,7 +4,7 @@
 
 # PART I
 # lab step #1: for loop that prints "hi"
-num = 10 # number of times to print "hi"
+num <- 10 # number of times to print "hi"
 
 for ( i in 1:num ) {
   print("hi")
@@ -22,8 +22,8 @@ for ( i in weeks ) {
 }
 
 # lab step #3: Population size
-percent = 0.05 # population shinkage per year
-current_size = 2000 # current population size
+percent <- 0.05 # population shinkage per year
+current_size <- 2000 # current population size
 years <- 1:7 # number of years of the study
 
 for (i in years ) {
@@ -32,15 +32,15 @@ for (i in years ) {
 }
 
 # lab step #4: logistic growth
-r = 0.8 # intrinsic growth rate
-K = 10000 # carrying capacity
+r <- 0.8 # intrinsic growth rate
+K <- 10000 # carrying capacity
 steps <- 2:12 # number of steps
 
 n <- rep(0, 12) # pre-allocated vector to store abundance data
 n[1] = 2500 # abundance at t = 1
 
 for ( t in steps ) {
-  n[t] = n[t-1] + ( r * n[t-1] * (K - n[t-1])/K ) # discrete logistic growth eq.
+  n[t] <- n[t-1] + ( r * n[t-1] * (K - n[t-1])/K ) # discrete logistic growth eq.
   print(n[t])
 }
 print("The abundance at time n[12]:")
@@ -81,12 +81,12 @@ plot(time, abundance)
 
 # lab step #8a: setting the working directory and reading the data
 setwd("~/Documents/EBIO_5420_CompBio/CompBioLabsAndHomework/Labs/Lab04")
-read.csv("CO2_data_cut_paste.csv")
+mydata <- read.csv("CO2_data_cut_paste.csv")
 
 # lab step #8b: converting "integer" types to "numeric" types
 
 # Method 1: Using the correct option in read.csv()
-mydata <- read.csv("CO2_data_cut_paste.csv", colClasses=c('integer','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+mydata <- read.csv("CO2_data_cut_paste.csv", colClasses=c('integer',rep('numeric',7)))
 
 # Method 2: Using a for loop
 numColumns <- length(mydata) # number of columns in mydata
